7ef8c44939e17f071f083c4071e71983207fd2f4
Jesus Gomez <jgomez@acsyt.com>
2025-12-02 15:27:10 -0600

    refactor: Clean up formatting and import statements in supplier-related components for consistency

--- src/app/_authenticated/suppliers/create.tsx
+++ src/app/_authenticated/suppliers/create.tsx
@@ -1,11 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router';
+
 import { ModeAction } from '@/config/enums/mode-action.enum';
 import { SuppliersFormContainer } from '@/features/suppliers/components/SupplierFormContainer';
-import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/suppliers/create')({
   component: RouteComponent
 });
 
 function RouteComponent() {
-  return <SuppliersFormContainer mode={ModeAction.Create} title='Crear proveedor' />
+  return (
+    <SuppliersFormContainer mode={ModeAction.Create} title='Crear proveedor' />
+  );
 }
